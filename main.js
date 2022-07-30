b753de81d954dac181e6dfd1d620baa8c666dd89
gilangaditya <gilangadtia712@gmail.com>
2022-07-30 00:44:53 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+'import index.html';
+
+
+var namaDepan;
+var namaBelakang;
+
+namaDepan='Gilang Aditya';
+namaBelakang='Purnama Sidhi';
+
+console.log(namaDepan);
+ 
+var a;
+var b;
+
+a = 10;
+b = 10;
+x = a*b;
+
+console.log(x);
+
+var a = ['mobil', 'motor', 'bajaj'];
+
+console.log(a[2]);
+
+var email;
+var password;
+ email= 'gilang@gmail.com';
+ password= '1234';
+ if(email == 'gilang@gmail' && password == '1234'){
+     alert('login sukses');
+ }else if(password == '1234'){
+     alert('email salah');
+ }else if(email == 'gilang@gmail'){
+     alert('password salah');
+ }else{
+     alert('login gagal');
+ }
